260a415a3ad6fc1a2e95d676c01d04aa8cbdc636
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2020-12-16 22:57:01 +0100

    csv-to-json v1.2

--- csv-to-json.sh
+++ csv-to-json.sh
@@ -1,17 +1,19 @@
-clear
+file=$1
 awk -F, ' \
-    BEGIN{print"{\n \"pokedex\":["}
-    {
-        if(NR==1){
-            for(i=1;i<=NF;i++)
-                fields[i]=$i
-        }
-
-        print"  {"
-        for(i in fields){
-            printf("   \"%s\":%s%s\n", fields[i], ($i ~ /^[0-9]+$/ ) ? $i : "\""$i"\"", (i!=NF) ? ",":"")
-        }
-        print ($1!=721) ? "  }," : "  }"
-    }
-    END{print" ]\n}"} ' \
-pokedexAll.csv
+NR==1{
+	for(i=1;i<=NF;i++)
+		fields[i]=$i
+	
+	print "{\n \""title"\":["
+}
+NR>1{
+	print "  {"
+	for(i in fields){
+		printf("   \"%s\":%s%s\n", fields[i], ($i ~ /^[0-9]+$/ ) ? $i : "\""$i"\"", (i!=NF) ? ",":"")
+	}
+	print (NR-1!=lines) ? "  }," : "  }"
+}
+END{print" ]\n}"} ' \
+lines=$(wc -l < $file) \
+title=$(echo $file | awk -F'.' '{print $1}') \
+$file
