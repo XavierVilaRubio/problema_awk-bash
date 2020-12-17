4fd0e189ced477ec0a7c577a2404aa38b381e353
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2020-12-17 12:09:14 +0100

    csv-to-json final

--- csv-to-json.sh
+++ csv-to-json.sh
@@ -1,21 +1,20 @@
 file=$1
-title=$(echo $file | awk -F'.' '{print $1}')
-awk -F, ' \
+title=${file%.csv}
+
+awk -v title="$title" -v lines="$(wc -l < $file)" -F, ' \
 NR==1{
-	for(i=1;i<=NF;i++)
-		fields[i]=$i
-	
-	print "{\n \""title"\":["
+    for(i=1;i<=NF;i++)
+        fields[i]=$i
+    
+    print "{\n \""title"\":["
 }
 NR>1{
-	print "  {"
-	for(i in fields){
-		printf("   \"%s\":%s%s\n", fields[i], ($i ~ /^[0-9]+$/ ) ? $i : "\""$i"\"", (i!=NF) ? ",":"")
-	}
-	print (NR-1!=lines) ? "  }," : "  }"
+    print "  {"
+    for(i=1;i<=NF;i++){
+        printf("   \"%s\":%s%s\n", fields[i], ($i ~ /^[0-9]+$/ ) ? $i : "\""$i"\"", (i!=NF) ? ",":"")
+    }
+    print (NR-1!=lines) ? "  }," : "  }"
 }
-END{print" ]\n}"NR lines} ' \
-lines=$(wc -l < $file) \
-title=$(echo $file | awk -F'.' '{print $1}') \
-$file > ${title}.json
+END{print" ]\n}"} ' $file > ${title}.json
+
 echo "The output is in ${title}.json"
